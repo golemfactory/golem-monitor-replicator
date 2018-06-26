c451accf157905947ef83c4ffc4f678f6bbabc4d
Piotr Chromiec <tworec@golem.network>
2018-06-26 20:42:25 +0200

    json to HashMap + bugfixes

--- src/stats_update.rs
+++ src/stats_update.rs
@@ -1,4 +1,3 @@
-
 use std::collections::HashMap;
 use serde_json::{Value, self};
 use serde::{Deserialize, Deserializer};
@@ -7,6 +6,7 @@ use std::marker::PhantomData;
 use serde::de::Visitor;
 use serde::de::MapAccess;
 use serde::de;
+use serde;
 use std::fmt;
 
 #[derive(Deserialize, Debug)]
@@ -109,7 +109,6 @@ enum StatsRequestBody {
         #[serde(default)]
         failed_total_time : f64
     },
-
     TaskComputer {
         #[serde(flatten)]
         extra: HashMap<String, Value>,
@@ -131,14 +130,6 @@ struct Metadata {
 
     #[serde(flatten)]
     extra: HashMap<String, Value>,
-}
-
-#[derive(Deserialize, Debug)]
-struct NodeInfo {
-    cliid : String,
-    timestamp : u64,
-    ip : Option<String>,
-    node_name : Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -214,7 +205,7 @@ fn string_or_struct<'de, T, D>(deserializer: D) -> Result<T, D::Error>
 }
 
 #[derive(Serialize, Debug)]
-struct OutputRecord {
+struct NodeInfoOutput {
     cliid : String,
     timestamp : f64,
     #[serde(flatten)]
@@ -282,35 +273,35 @@ struct StatsOutput {
 #[derive(Serialize, Debug, Default)]
 struct RequestorStatsOutput {
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_tasks_cnt : Option<u64>,
+    rs_tasks_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_finished_task_cnt : Option<u64>,
+    rs_finished_task_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_requested_subtasks_cnt : Option<u64>,
+    rs_requested_subtasks_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_collected_results_cnt : Option<u64>,
+    rs_collected_results_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_verified_results_cnt : Option<u64>,
+    rs_verified_results_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_timed_out_subtasks_cnt : Option<u64>,
+    rs_timed_out_subtasks_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_not_downloadable_subtasks_cnt : Option<u64>,
+    rs_not_downloadable_subtasks_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_failed_subtasks_cnt : Option<u64>,
+    rs_failed_subtasks_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_work_offers_cnt : Option<u64>,
+    rs_work_offers_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_finished_ok_cnt : Option<u64>,
+    rs_finished_ok_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_finished_ok_total_time : Option<f64>,
+    rs_finished_ok_total_time : Option<f64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_finished_with_failures_cnt : Option<u64>,
+    rs_finished_with_failures_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_finished_with_failures_total_time : Option<f64>,
+    rs_finished_with_failures_total_time : Option<f64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_failed_cnt : Option<u64>,
+    rs_failed_cnt : Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    req_failed_total_time : Option<f64>
+    rs_failed_total_time : Option<f64>
 }
 
 fn protocol_versions_to_map(protocol_versions: &HashMap<String, Value>) -> HashMap<String, Value> {
@@ -319,13 +310,12 @@ fn protocol_versions_to_map(protocol_versions: &HashMap<String, Value>) -> HashM
         .collect()
 }
 
-fn get_update_node(env: Envelope<StatsRequest>) -> Option<OutputRecord> {
+fn to_node_info(env: Envelope<StatsRequest>) -> Option<NodeInfoOutput> {
     let StatsRequest { cliid, timestamp, sessid: _, body } = env.data;
-
 
     match body {
         StatsRequestBody::Login { metadata, protocol_versions, .. } =>
-            Some(OutputRecord {
+            Some(NodeInfoOutput {
                 cliid,
                 timestamp,
                 extra: protocol_versions_to_map(&protocol_versions),
@@ -362,28 +352,28 @@ fn get_update_node(env: Envelope<StatsRequest>) -> Option<OutputRecord> {
             finished_with_failures_total_time,
             failed_cnt,
             failed_total_time
-        } => Some(OutputRecord {
+        } => Some(NodeInfoOutput {
             cliid,
             timestamp,
             metadata: MedataOutput::default(),
             extra: HashMap::new(),
             stats: StatsOutput::default(),
             requestor_stats: RequestorStatsOutput {
-                req_tasks_cnt: Some(tasks_cnt),
-                req_finished_task_cnt: Some(finished_task_cnt),
-                req_requested_subtasks_cnt: Some(requested_subtasks_cnt),
-                req_collected_results_cnt: Some(collected_results_cnt),
-                req_verified_results_cnt: Some(verified_results_cnt),
-                req_timed_out_subtasks_cnt: Some(timed_out_subtasks_cnt),
-                req_not_downloadable_subtasks_cnt: Some(not_downloadable_subtasks_cnt),
-                req_failed_cnt: Some(failed_cnt),
-                req_failed_subtasks_cnt: Some(failed_subtasks_cnt),
-                req_failed_total_time: Some(failed_total_time),
-                req_finished_ok_cnt: Some(finished_ok_cnt),
-                req_finished_ok_total_time: Some(finished_ok_total_time),
-                req_finished_with_failures_cnt: Some(finished_with_failures_cnt),
-                req_finished_with_failures_total_time: Some(finished_with_failures_total_time),
-                req_work_offers_cnt: Some(work_offers_cnt),
+                rs_tasks_cnt: Some(tasks_cnt),
+                rs_finished_task_cnt: Some(finished_task_cnt),
+                rs_requested_subtasks_cnt: Some(requested_subtasks_cnt),
+                rs_collected_results_cnt: Some(collected_results_cnt),
+                rs_verified_results_cnt: Some(verified_results_cnt),
+                rs_timed_out_subtasks_cnt: Some(timed_out_subtasks_cnt),
+                rs_not_downloadable_subtasks_cnt: Some(not_downloadable_subtasks_cnt),
+                rs_failed_cnt: Some(failed_cnt),
+                rs_failed_subtasks_cnt: Some(failed_subtasks_cnt),
+                rs_failed_total_time: Some(failed_total_time),
+                rs_finished_ok_cnt: Some(finished_ok_cnt),
+                rs_finished_ok_total_time: Some(finished_ok_total_time),
+                rs_finished_with_failures_cnt: Some(finished_with_failures_cnt),
+                rs_finished_with_failures_total_time: Some(finished_with_failures_total_time),
+                rs_work_offers_cnt: Some(work_offers_cnt),
             },
         }),
         StatsRequestBody::Stats {
@@ -394,7 +384,7 @@ fn get_update_node(env: Envelope<StatsRequest>) -> Option<OutputRecord> {
             tasks_with_timeout,
             tasks_requested,
             ..
-        } => Some(OutputRecord {
+        } => Some(NodeInfoOutput {
             cliid,
             timestamp,
             metadata: MedataOutput::default(),
@@ -432,32 +422,56 @@ pub struct UpdateHandler {
 
 impl UpdateHandler {
     pub fn new(redis_actor : Addr<Unsync, RedisActor>) -> UpdateHandler {
-
-
         UpdateHandler {
             updater : Updater::start(redis_actor)
         }
     }
-
 }
 
-fn update_kv(updater : &Addr<Unsync, Updater>, output : &OutputRecord) -> Box<Future<Item = HttpResponse, Error=actix_web::Error>> {
-    let key = format!("node.{}", output.cliid);
-    //let script = "local t1 = cjson.decode(redis.call('get', ARGV[1])); local t2 = cjson.decode(ARGV[2]); for k,v in pairs(t2) do t1[k] = v end; return redis.call('set', ARGV[1], cjson.encode(t1));";
-    if let Ok(json) = serde_json::to_string(&output) {
-        return Box::new(
-            updater.send(UpdateKey { key, value: json })
-                .map_err(|_e| actix_web::error::ErrorInternalServerError("send error"))
-                .and_then(|r| match r {
-                    Ok(_v) => {
-                        future::ok(HttpResponse::Ok().into())
-                    },
-                    Err(e) => future::err(actix_web::error::ErrorInternalServerError(format!("save: {}", e)))
-                })
-        )
-    }
+#[derive(Debug)]
+enum ConvertError {
+    JSONError(serde_json::Error),
+    InvalidJson
+}
 
-    Box::new(future::err(actix_web::error::ErrorInternalServerError("gen output")))
+impl From<serde_json::Error> for ConvertError {
+    fn from(err: serde_json::Error) -> Self {
+        ConvertError::JSONError(err)
+    }
+}
+
+fn to_hash_map<T : serde::Serialize>(input : T) -> Result<HashMap<String, String>, ConvertError> {
+    if let serde_json::Value::Object(map) = serde_json::to_value(input)? {
+        Ok(map
+            .iter()
+            .filter_map(|(k, v)|
+                match v {
+                    serde_json::Value::String(s) => Some((k.clone(), s.clone())),
+                    serde_json::Value::Number(n) => Some((k.clone(), n.to_string())),
+                    serde_json::Value::Bool(b) => Some((k.clone(), format!("{}", b))),
+                    _ => None
+            })
+            .collect()
+        )
+    } else {
+        Err(ConvertError::InvalidJson)
+    }
+}
+
+fn update_kv(updater : &Addr<Unsync, Updater>, nodeInfo : &NodeInfoOutput)
+        -> Box<Future<Item = HttpResponse, Error=actix_web::Error>> {
+    let key = format!("nodeinfo.{}", nodeInfo.cliid);
+
+    if let Ok(map) = to_hash_map(nodeInfo) {
+        Box::new(updater.send(UpdateKey { key, value: map })
+            .map_err(|_e| actix_web::error::ErrorInternalServerError("send error"))
+            .and_then(|r| match r {
+                Ok(_v) => future::ok(HttpResponse::Ok().into()),
+                Err(e) => future::err(actix_web::error::ErrorInternalServerError(format!("save: {}", e)))
+            }))
+    } else {
+        Box::new(future::err(actix_web::error::ErrorInternalServerError("gen nodeInfo")))
+    }
 }
 
 impl Handler<()> for UpdateHandler {
@@ -466,17 +480,17 @@ impl Handler<()> for UpdateHandler {
     fn handle(&mut self, req: HttpRequest<()>) -> <Self as Handler<()>>::Result {
         let updater = self.updater.clone();
 
-        req.json()
-            .map_err(|e| actix_web::error::ErrorBadRequest(format!("{}", e)))
+        req.json() // here json is converted to Envelope<StatsRequests>
+            .from_err()
             .and_then(|b:Envelope<StatsRequest>|
-                Ok(get_update_node(b))
+                Ok(to_node_info(b))
             )
             .and_then(move |r| {
-                let out : Box<Future<Item = HttpResponse, Error=actix_web::Error>> = match r {
-                    Some(output) => update_kv(&updater, &output),
-                    None => Box::new(future::ok(HttpResponse::Ok().into()))
-                };
-                out
+                if let Some(nodeInfo) = r {
+                    update_kv(&updater, &nodeInfo)
+                } else {
+                    Box::new(future::ok(HttpResponse::Ok().into()))
+                }
             }).or_else(|e : actix_web::Error| {
                 let mut resp = e.cause().error_response();
                 warn!("processing request, error={:?}", &e);
@@ -484,7 +498,6 @@ impl Handler<()> for UpdateHandler {
                 future::ok(resp)
             })
             .responder()
-
     }
 }
 
@@ -502,7 +515,7 @@ mod tests {
         let r: Envelope<StatsRequest> = serde_json::from_str(input).unwrap();
 
         {
-            println!("{:?}", &r);
+            println!("envl {:?}", &r);
             let settings: &Settings = match &r.data.body {
                 &StatsRequestBody::Login { ref metadata, .. } => &metadata.as_ref().unwrap().settings,
                 _ => panic!("login expected")
@@ -510,31 +523,29 @@ mod tests {
             assert_eq!(&settings.extra["use_ipv6"], &json!(0));
         }
 
-        let output = get_update_node(r);
-        println!("{}", serde_json::to_string_pretty(&output.unwrap()).unwrap());
+        let output = to_node_info(r);
+        println!("pretty json {}", serde_json::to_string_pretty(&output.unwrap()).unwrap());
     }
 
     #[test]
     fn parse_stats() {
         let input = include_str!("../test/stats.json");
-
-
-        let r: Envelope<StatsRequest> = serde_json::from_str(input).unwrap();
-
-        let output = get_update_node(r);
-        println!("{}", serde_json::to_string_pretty(&output.unwrap()).unwrap());
+        let output = to_node_info(serde_json::from_str(input).unwrap());
+        println!("pretty json {}", serde_json::to_string_pretty(&output.unwrap()).unwrap());
     }
 
 
     #[test]
     fn parse_requestor_stats() {
         let input = include_str!("../test/requestor-stats.json");
-
-
-        let r: Envelope<StatsRequest> = serde_json::from_str(input).unwrap();
-
-        let output = get_update_node(r);
-        println!("{}", serde_json::to_string_pretty(&output.unwrap()).unwrap());
+        let output = to_node_info(serde_json::from_str(input).unwrap());
+        println!("pretty json {}", serde_json::to_string_pretty(&output.unwrap()).unwrap());
     }
 
+    #[test]
+    fn parse_stats_output() {
+        let input = include_str!("../test/stats.json");
+        let map = to_hash_map(to_node_info(serde_json::from_str(input).unwrap())).unwrap();
+        println!("pretty json {:?}", map);
+    }
 }
--- src/updater.rs
+++ src/updater.rs
@@ -1,34 +1,21 @@
 use futures::prelude::*;
-use futures::future;
 use actix::prelude::*;
-use actix::fut;
-use actix_redis::{RedisActor, RespValue, Command, self};
-
-const UPDATE_SCRIPT: &str = r#"
-local t1 = cjson.decode(redis.call('get', KEYS[1]) or '{}');
-local t2 = cjson.decode(ARGV[1]); for k,v in pairs(t2) do t1[k] = v end;
-return redis.call('set', KEYS[1], cjson.encode(t1))
-"#;
-
+use actix_redis::{RedisActor, RespValue, Command};
+use std::collections::HashMap;
 
 pub struct Updater {
     redis_actor: Addr<Unsync, RedisActor>,
-    script_hash: Option<RespValue>,
 }
 
 impl Updater {
     pub fn start(redis_actor: Addr<Unsync, RedisActor>) -> Addr<Unsync, Updater> {
-        Supervisor::start(|_| Updater {
-            redis_actor,
-            script_hash: None
-        })
+        Supervisor::start(|_| Updater {redis_actor})
     }
 }
 
-
 pub struct UpdateKey {
     pub key: String,
-    pub value: String,
+    pub value: HashMap<String, String>,
 }
 
 impl Message for UpdateKey {
@@ -39,24 +26,13 @@ impl Actor for Updater {
     type Context = Context<Self>;
 
     fn started(&mut self, ctx: &mut <Self as Actor>::Context) {
-        self.redis_actor
-            .send(Command(resp_array!["SCRIPT","LOAD", UPDATE_SCRIPT]))
+        self.redis_actor.send(
+            Command(resp_array!["CLIENT", "SETNAME",
+                    format!("monitor-thread-{:?}", ::std::thread::current().id())
+                  ]))
+            .map(|_r| ())
+            .map_err(|e| warn!("CLIENT SETNAME error {:?}", e))
             .into_actor(self)
-            .and_then(|res: Result<RespValue, actix_redis::Error>, act: &mut Updater, ctx: &mut <Updater as Actor>::Context| {
-                debug!("script load result {:?}", &res);
-                match res {
-                    Ok(h) => { act.script_hash = Some(h); }
-                    Err(ref err) => error!("script load error: {:?}", err)
-                };
-                ctx.spawn(
-                    act.redis_actor.send(Command(resp_array!["CLIENT","SETNAME", format!("monitor-thread-{:?}", ::std::thread::current().id())]))
-                        .map(|_r| ())
-                        .map_err(|e| warn!("CLIENT SETNAME error {:?}", e))
-                        .into_actor(act)
-                );
-                fut::ok(())
-            })
-            .map_err(|_e, _act, _ctx| ())
             .wait(ctx);
 
         //ctx.spawn(r);
@@ -71,17 +47,8 @@ impl Supervised for Updater {
 
 #[derive(Fail, Debug)]
 pub enum Error {
-    #[fail(display = "Redis error {}", _0)]
-    Redis(actix_redis::Error),
-
     #[fail(display = "MailboxError {}", _0)]
     MailboxError(actix::MailboxError),
-
-    #[fail(display = "ScriptError {}", _0)]
-    ScriptError(String),
-
-    #[fail(display = "NoScript")]
-    NoScript
 }
 
 
@@ -91,56 +58,27 @@ impl From<MailboxError> for Error {
     }
 }
 
-fn eval_if_hash(redis : &Addr<Unsync, RedisActor>, hash : &Option<RespValue>, k : String, v : String)
-    -> Box<Future<Item=RespValue, Error=Error>> {
-
-    match hash.as_ref() {
-        Some(hash) => Box::new(
-            redis.send(Command(resp_array!["EVALSHA", hash.clone(), "1", &k, &v]))
-                .map_err(|e| Error::MailboxError(e))
-                .and_then(|r| {
-                    match r {
-                        Ok(RespValue::Error(err)) =>
-                            if err.starts_with("NOSCRIPT") {
-                                future::err(Error::NoScript)
-                            } else {
-                                future::err(Error::ScriptError(err))
-                            },
-                        Err(err) => future::err(Error::Redis(err)),
-                        Ok(other) => future::ok(other)
-                    }
-                })),
-        None => Box::new(redis.send(Command(resp_array!["EVAL", UPDATE_SCRIPT, "1", &k, &v]))
-            .map_err(|e| Error::MailboxError(e))
-            .and_then(|r| {
-                match r {
-                    Ok(RespValue::Error(err)) => future::err(Error::ScriptError(err)),
-                    Err(err) => future::err(Error::Redis(err)),
-                    Ok(other) => future::ok(other)
-                }
-            }))
-    }
-}
-
 
 impl Handler<UpdateKey> for Updater {
     type Result = ActorResponse<Updater, (), Error>;
 
     fn handle(&mut self, msg: UpdateKey, _: &mut Self::Context) -> <Self as Handler<UpdateKey>>::Result {
         let redis_actor = &self.redis_actor;
+        let mut msg_vec: Vec<RespValue> = Vec::with_capacity(2 + msg.value.len() * 2);
+        msg_vec.push(RespValue::SimpleString("HMSET".into()));
+        msg_vec.push(RespValue::SimpleString(msg.key));
 
-        let f =
-            eval_if_hash(redis_actor, &self.script_hash, msg.key, msg.value)
+        for (key, value) in msg.value {
+            msg_vec.push(RespValue::SimpleString(key));
+            msg_vec.push(RespValue::SimpleString(value));
+        }
+
+
+        let f = redis_actor.send(Command(RespValue::Array(msg_vec)))
                 .into_actor(self)
-                .map_err(|e, _act : &mut Updater, ctx : &mut <Updater as Actor>::Context|{
+                .map_err(|e, _, _|{
                     error!("update key error {:?}", &e);
-                    match e {
-                        Error::NoScript => {
-                            ctx.stop();
-                            e
-                        }
-                        _ => e
-                    }
+                    e.into()
                 })
                 .map(|_,_,_| ());
 
